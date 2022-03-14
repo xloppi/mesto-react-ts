150a3af190df8988eaf3916a57d5be47beb4601b
xloppi <hlopkovalesha@yandex.ru>
2022-03-14 17:53:04 +0300

    add modal

---
+++ src/components/PopupWithForm/PopupWithForm.tsx
@@ -0,0 +1,11 @@
+import { StyledPopupWithForm } from "./PopupWithForm.styles";
+
+const PopupWithForm = () => {
+  return(
+    <StyledPopupWithForm>
+
+    </StyledPopupWithForm>
+  )
+}
+
+export default PopupWithForm;
